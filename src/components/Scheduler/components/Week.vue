28a0a8ff0a611e83a12bdca849f30eb8a30f2a37
Guilherme Moura <mouraggui@gmail.com>
2022-11-15 10:29:06 -0300

    Week component

---
+++ src/components/Scheduler/components/Week.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr class="calendar__week">
+    <slot />
+  </tr>
+</template>
+
+<style lang="scss" scoped>
+.calendar__week {
+  flex-grow: 1;
+  display: grid;
+  grid-gap: 1px;
+  grid-template-columns: repeat(7, minmax(100px, 1fr));
+  grid-template-rows: repeat(1, minmax(140px, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(7, minmax(140px, 1fr));
+  }
+}
+</style>
