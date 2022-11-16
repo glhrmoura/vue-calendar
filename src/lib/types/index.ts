9ff2a3beb9959d42a0181325a489ee0dd95d5d34
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 14:54:51 -0300

    feat: adding type folder

---
+++ src/lib/types/index.ts
@@ -0,0 +1,33 @@
+export interface Interval {
+  from: string;
+  to: string;
+}
+
+export interface Range {
+  start?: Date;
+  end?: Date;
+}
+
+export interface WeekDayEvent {
+  date: Date;
+}
+
+export interface Rule {
+  date?: Date;
+  wday?: string;
+  intervals: Interval[];
+}
+
+export interface WeekDay {
+  date: Date;
+  rule?: Rule;
+}
+
+export interface SchedulerData {
+  dates: Date[];
+  selectedRange: Date[];
+  selectionOrigin: Date | null;
+  inMutipleSelection: boolean;
+  calenderStartDayData: Date;
+  weekDays: string[];
+}
