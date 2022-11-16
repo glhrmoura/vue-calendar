8f9ef2db4eef3cffdef19a6e9559106cc486dd46
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 15:03:05 -0300

    feat: change outputDir

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: false,
   productionSourceMap: false,
+  outputDir: path.resolve(__dirname, './lib'),
   css: {
     extract: false,
   },
