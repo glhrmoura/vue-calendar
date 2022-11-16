f3ee7a114827c09642ee68315efea450ba339f6b
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 14:49:33 -0300

    feat: adding new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'quotes': ['error', 'single'],
+    'eol-last': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off'
