ccb24b23c9ff27ac95082993776631712b37886e
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 14:55:22 -0300

    fix: entry app code adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <Scheduler
-    :range="{ start: new Date('11/05/2022'), end: new Date('11/22/2022') }"
-    :rules="[
-      {
-        date: new Date('11/17/2021'),
-        intervals: [
-          { from: '09:20', to: '12:45' }
-        ]
-      }
-    ]"
+    :range="range"
+    :rules="rules"
   />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Scheduler from '@/components/Scheduler/index.vue';
+
+import Scheduler from '@/components/Scheduler.vue';
 
 export default defineComponent({
   name: 'App',
+
   components: {
     Scheduler,
   },
+
+  data: () => ({
+    range: {
+      start: new Date('11/05/2022'),
+      end: new Date('11/22/2022'),
+    },
+
+    rules: [
+      {
+        date: new Date('11/17/2022'),
+        intervals: [
+          { from: '09:20', to: '12:45' }
+        ]
+      }
+    ],
+  }),
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
