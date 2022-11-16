6193e4bba47c916adc82a8309b3f62159e99e7c9
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 14:52:51 -0300

    feat: create Button component

---
+++ src/lib/components/Button.vue
@@ -0,0 +1,58 @@
+<template>
+  <button
+    @click="$emit('click', $event)"
+    :class="{
+      'button__link': typeIs('link'),
+      'button__arrow': typeIs('arrow'),
+    }"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    label: String,
+
+    type: {
+      type: String,
+      default: 'link',
+    }
+  },
+
+  methods: {
+    typeIs(type: string): boolean {
+      return this.type === type;
+    },
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.button__link {
+  background-color: transparent;
+  border: none;
+  outline: none;
+  text-decoration: underline;
+  font-size: 14px;
+  margin-right: 12px;
+}
+
+.button__arrow {
+  background-color: transparent;
+  border: none;
+  outline: none;
+  transition: all 0.1s;
+
+  &:active {
+    transform: scale(0.8);
+  }
+
+  &:not(:last-child) {
+    margin-right: 12px;
+  }
+}
+</style>
