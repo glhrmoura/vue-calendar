1ed198ccc32ab3af5258358f6d3723c3ba841279
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 16:26:05 -0300

    feat: create Interval component

---
+++ src/lib/components/Interval.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="interval" class="vue-scheduler__week-day__event">
+    {{ interval.from }} - {{ interval.to }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+import type { Interval } from '@/types';
+
+export default defineComponent({
+  props: {
+    interval: Object as PropType<Interval>,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.vue-scheduler__week-day__event {
+  background-color: inherit;
+  border-color: green;
+  color: blue;
+  border-radius: 44px;
+  padding: 4px 0px;
+  text-align: center;
+  border: 1px solid red;
+
+  &:not(:first-child) {
+    margin-top: 8px;
+  }
+
+  @media (max-width: 768px) {
+    text-align: center;
+    padding: 8px 8px;
+  }
+}
+</style>
