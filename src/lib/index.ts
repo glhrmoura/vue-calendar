bdb8d7cc26656dd9111845d549ff24a8398680aa
Guilherme Moura <mouraggui@gmail.com>
2022-11-16 21:57:49 -0300

    feat: typing lib index file

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+import { App, Plugin } from 'vue'
+
+import Scheduler from '@/components/Scheduler.vue';
+
+export const VueSchedulerPlugin = {
+  install(Vue: App) {
+    Vue.component('VueScheduler', Scheduler);
+  }
+} as Plugin;
+
+export default Scheduler;
