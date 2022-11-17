e9c151a5531abb95628d78ae6cf5f92f8c8de3a2
Guilherme Moura <mouraggui@gmail.com>
2022-11-17 09:11:54 -0300

    feat: adding semi config in lint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,11 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'semi': ['error', 'always'],
     'quotes': ['error', 'single'],
     'eol-last': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off'
   }
-}
+};
