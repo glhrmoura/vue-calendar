4ca75c3324acc3b3d9f21c7ac0110e0d3aab63b4
Guilherme Moura <mouraggui@gmail.com>
2022-11-17 09:15:47 -0300

    feat: adding comma-dangle=always-multiline config in lint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'semi': ['error', 'always'],
     'quotes': ['error', 'single'],
     'eol-last': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 'off'
-  }
+    'vue/multi-word-component-names': 'off',
+  },
 };
