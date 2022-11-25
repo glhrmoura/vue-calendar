7c3a0552e9e74231badfe7e9787a7c45ea4a0af3
Guilherme Moura <mouraggui@gmail.com>
2022-11-25 09:59:22 -0300

    fix(Event): styles update

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="event" class="vue-scheduler__week-day__event">
+  <div v-if="event" class="week-day-event">
     <Interval :interval="event.interval" />
   </div>
 </template>
@@ -25,13 +25,13 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.vue-scheduler__week-day__event {
+.week-day-event {
   background-color: inherit;
-  color: #cfcaca;
-  border-radius: 44px;
+  color: #A8A8A8;
+  border-radius: 4px;
   padding: 4px 0px;
   text-align: center;
-  border: 1px solid #cfcaca;
+  border: 1px solid #A8A8A8;
 
   @media (max-width: 768px) {
     text-align: center;
