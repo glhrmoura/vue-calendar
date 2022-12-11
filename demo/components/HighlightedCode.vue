4c9a33e055880077bd424ade86f74f3d104c66a2
Guilherme Moura <glhrmoura@gmail.com>
2022-12-11 13:19:10 -0300

    feat: HighlightedCode component

---
+++ demo/components/HighlightedCode.vue
@@ -0,0 +1,23 @@
+<template>
+  <pre v-highlightjs>
+    <code class="html">
+      <template v-if="$slots.template">
+        &lt;template&gt;
+          <slot name="template" />
+        &lt;/template&gt;
+      </template>
+
+      <template v-if="$slots.script">
+        &lt;script&gt;
+          <slot name="script" />
+        &lt;/script&gt;
+      </template>
+
+      <template v-if="$slots.style">
+        &lt;style&gt;
+          <slot name="style" />
+        &lt;/style&gt;
+      </template>
+    </code>
+  </pre>
+</template>
