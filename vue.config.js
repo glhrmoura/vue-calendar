8b47ad1bba0494d0e86bf59941be59e363e3669b
Guilherme Moura <mouraggui@gmail.com>
2023-05-07 16:04:34 -0300

    refactor(package): publish config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   productionSourceMap: false,
   transpileDependencies: false,
-  outputDir: path.resolve(__dirname, './lib'),
+  outputDir: path.resolve(__dirname, './dist'),
   css: {
     extract: false,
   },
