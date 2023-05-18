9ad0f5d79f8d1a8c7a98f0372ec673e7a36e0db3
Guilherme Moura <mouraggui@gmail.com>
2023-05-18 16:51:38 -0300

    refactor: rename file

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 
 import 'vue-highlight-code/dist/style.css';
 
-import IndexApp from './index.vue';
+import App from './app.vue';
 
 globalThis.__VUE_OPTIONS_API__ = true
 globalThis.__VUE_PROD_DEVTOOLS__ = false;
 
-createApp(IndexApp).mount('#app');
+createApp(App).mount('#app');
